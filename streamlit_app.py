c5b85702a516dcec737ad3e1d5ee1dfc4f406b17
Rmahadarcode <152143436+Rmahadarcode@users.noreply.github.com>
2023-11-29 11:40:53 +0530

    Update streamlit_app.py
    Impored request library and added api response for streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 ## streamlit.multiselect("Pick some fruits:", list(my_fruit_list['Fruit']))
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+#New section to display fruityvice api response 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
